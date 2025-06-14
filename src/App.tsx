321e0155a879f124020531b869b570b025afe9c9
NezraDev <nezraprogrammer@gmail.com>
2025-06-14 21:38:52 +0800

    refactor forgot page

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // Auth
 import Login from "./pages/auth/login.tsx";
 import Forgot from "./pages/auth/forgot.tsx";
-/** import Register from "./pages/auth/register.tsx"; **/
+import Register from "./pages/auth/register.tsx";
 import Reset from "./pages/auth/reset.tsx";
 
 const App: React.FC = () => {
@@ -13,7 +13,7 @@ const App: React.FC = () => {
       <Routes>
         {/* Auth */}
         <Route path="/login" element={<Login />} />
-        {/* <Route path="/register" element={<Register />} />*/}
+        <Route path="/register" element={<Register />} />
         <Route path="/forgot" element={<Forgot />} />
         <Route path="/reset" element={<Reset />} />
       </Routes>
--- src/pages/auth/forgot.tsx
+++ src/pages/auth/forgot.tsx
@@ -1,11 +1,14 @@
+import ThemedButton from "../../components/ThemedButton.tsx";
+import ThemedInput from "../../components/ThemedInput.tsx";
 import { EnvelopeIcon } from "@heroicons/react/24/solid";
+import { Link } from "react-router-dom";
 
 const Forgot = () => {
   return (
-    <main className="bg-peach-tint-300 font-quicksand flex h-screen items-center justify-center text-sm">
-      <div className="mx-auto flex w-full max-w-4xl overflow-hidden rounded-lg shadow-2xl">
-        <div className="bg-peach-tint-500 flex flex-1 items-center justify-center p-8 text-center">
-          <div className="bg-peach-tint-400 rounded-md px-4 py-8 shadow-lg">
+    <main className="bg-peach-tint-300 font-quicksand flex h-screen items-center justify-center text-xs">
+      <div className="flex h-full w-full max-w-3xl flex-col overflow-hidden rounded-lg shadow-2xl md:h-auto md:flex-row">
+        <div className="md:bg-peach-tint-500 bg-peach-tint-300 flex flex-1 items-center justify-center p-8 text-center">
+          <div className="bg-peach-tint-400 flex h-full max-h-44 flex-col items-center justify-center rounded-md p-8 px-4 py-8 shadow-lg">
             <div className="mb-4 text-xl font-semibold">Life, Unfiltered.</div>
             <p>
               Life isn’t always easy, but it always teaches. Every struggle has
@@ -20,48 +23,29 @@ const Forgot = () => {
             alt="Rant on me logo"
           />
           <div>
-            <p className="font-semibold">Forgot Password</p>
-            <p className="mt-1 text-xs text-gray-600">
+            <p className="text-sm font-semibold">Forgot Password</p>
+            <p className="text-gray-600">
               Enter your email and we'll help you get back to venting
             </p>
           </div>
           <form className="flex flex-col gap-4" action="">
-            <div className="flex flex-col gap-3">
-              {/* Email Input */}
-              <label
-                htmlFor="email"
-                className="flex cursor-pointer items-center gap-2 rounded-sm border border-gray-300 p-1"
-              >
-                <div className="ml-2 flex items-center">
-                  <EnvelopeIcon className="text-accent h-5 w-5" />
-                </div>
-                <div className="relative w-full">
-                  <input
-                    id="email"
-                    type="email"
-                    required
-                    placeholder=" "
-                    className="peer ml-1 w-full border-none bg-white pt-6 pb-1 text-sm font-semibold text-black placeholder-transparent focus:outline-none"
-                  />
-                  <span className="absolute top-1 left-1 text-xs text-black transition-all peer-placeholder-shown:top-3.5 peer-placeholder-shown:text-sm peer-placeholder-shown:text-gray-400 peer-focus:top-1 peer-focus:text-xs peer-focus:text-black">
-                    Email Address
-                  </span>
-                </div>
-              </label>
-            </div>
-            {/* Submit Button */}
-            <input
-              className="bg-accent cursor-pointer rounded p-2 text-white"
-              type="submit"
-              value="Send Email"
+            <ThemedInput
+              label="Email Address"
+              type="email"
+              id="email"
+              autoComplete="email"
+              icon={<EnvelopeIcon className="text-accent h-5 w-5" />}
             />
+            <ThemedButton type="submit" value="Send Email" />
           </form>
-
-          <div className="mt-4 flex justify-center gap-2">
+          <div className="flex justify-center gap-1">
             <p>Ready to vent?</p>
-            <a className="underline" href="">
+            <Link
+              to="/register"
+              className="text-accent font-semibold underline"
+            >
               Create an Account
-            </a>
+            </Link>
           </div>
         </div>
       </div>
