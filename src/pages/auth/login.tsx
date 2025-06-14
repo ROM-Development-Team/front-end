c31752e220a35518d80674ed21b7e3c5fb4688d4
Christian <prog.christian07@gmail.com>
2025-06-14 14:01:45 +0800

    feat: finalize login page

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,8 +1,11 @@
 import { Link } from "react-router-dom";
+import ThemedButton from "../../components/ThemedButton.tsx";
+import ThemedInput from "../../components/ThemedInput.tsx";
+import { EnvelopeIcon, LockClosedIcon } from "@heroicons/react/24/solid";
 
 const Login = () => {
   return (
-    <main className="bg-peach-tint-300 font-quicksand flex h-screen items-center justify-center text-sm">
+    <main className="bg-peach-tint-300 font-quicksand flex h-screen items-center justify-center text-xs">
       <div className="flex h-full w-full max-w-3xl flex-col overflow-hidden rounded-lg shadow-2xl md:h-auto md:flex-row">
         <div className="md:bg-peach-tint-500 bg-peach-tint-300 flex flex-1 items-center justify-center p-8 text-center">
           <div className="bg-peach-tint-400 flex h-full max-h-44 flex-col items-center justify-center rounded-md p-8 px-4 py-8 shadow-lg">
@@ -20,38 +23,44 @@ const Login = () => {
             alt="Rant on me logo"
           />
           <div>
-            <p className="text-base">Welcome Back, Time to vent again?</p>
-            <p>Please enter log in details below</p>
+            <p className="text-sm font-semibold">
+              Welcome Back, Time to vent again?
+            </p>
+            <p className="text-gray-600">Please enter log in details below</p>
           </div>
           <form className="flex flex-col gap-4" action="">
-            <label htmlFor="">
-              <input
-                className="w-full border bg-white p-2"
-                type="text"
-                placeholder="Email Address"
-              />
-            </label>
-            <label htmlFor="">
-              <input
-                className="w-full border bg-white p-2"
-                type="text"
-                placeholder="Password"
-              />
-            </label>
-            <Link to="/forgot" className="self-end underline">
+            <ThemedInput
+              label="Email Address"
+              type="email"
+              id="email"
+              autoComplete="email"
+              icon={<EnvelopeIcon className="text-accent h-5 w-5" />}
+            />
+            <ThemedInput
+              label="Password"
+              type="password"
+              id="password"
+              icon={<LockClosedIcon className="text-accent h-5 w-5" />}
+            />
+
+            <Link
+              to="/forgot"
+              className="text-accent self-end font-semibold underline"
+            >
               Forgot Password?
             </Link>
-            <input
-              className="bg-accent cursor-pointer p-2 text-white"
-              type="submit"
-              value="Sign in"
-            />
+            <ThemedButton type="submit" value="Sign in" />
           </form>
           <p className="text-center">or</p>
           <p className="text-center">Sign in with Google</p>
-          <div className="flex justify-center gap-2">
+          <div className="flex justify-center gap-1">
             <p>Ready to vent?</p>
-            <Link to="/register">Create an Account</Link>
+            <Link
+              to="/register"
+              className="text-accent font-semibold underline"
+            >
+              Create an Account
+            </Link>
           </div>
         </div>
       </div>
