963441239483da72b2f06b89b18465ed5f20f1ef
NezraDev <nezraprogrammer@gmail.com>
2025-06-14 23:47:00 +0800

    decrease the size of input

--- src/components/ThemedInput.tsx
+++ src/components/ThemedInput.tsx
@@ -17,22 +17,23 @@ const ThemedInput = ({
   return (
     <label
       htmlFor={id}
-      className={`flex cursor-pointer items-center gap-2 rounded-sm border border-gray-300 p-1 ${containerClassName}`}
+      className={`flex cursor-pointer items-center gap-2 rounded-sm border border-gray-300 px-2 py-1.5 text-xs ${containerClassName}`}
     >
-      <div className="ml-2 flex items-center">{icon}</div>
+      <div className="flex items-center">{icon}</div>
       <div className="relative w-full">
         <input
           id={id}
           required
           placeholder=" "
-          className={`peer ml-1 w-full border-none bg-white pt-6 pb-1 text-sm font-semibold text-black placeholder-transparent focus:outline-none ${className}`}
+          className={`peer ml-1 w-full border-none bg-white pt-4.5 pb-1 text-xs font-medium text-black placeholder-transparent focus:outline-none ${className}`}
           {...rest}
         />
-        <span className="absolute top-1 left-1 text-xs text-black transition-all peer-placeholder-shown:top-3.5 peer-placeholder-shown:text-sm peer-placeholder-shown:text-gray-400 peer-focus:top-1 peer-focus:text-xs peer-focus:text-black">
+        <span className="absolute top-1 left-1 text-[10px] text-black transition-all peer-placeholder-shown:top-3 peer-placeholder-shown:text-xs peer-placeholder-shown:text-gray-400 peer-focus:top-1 peer-focus:text-[10px] peer-focus:text-black">
           {label}
         </span>
       </div>
     </label>
   );
 };
+
 export default ThemedInput;
